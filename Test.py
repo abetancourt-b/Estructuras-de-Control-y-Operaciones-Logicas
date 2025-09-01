5640303396527df68192ebb3f6b7c602123786e8
Antonio Betancourt <abetancourtt@unal.edu.co>
2025-09-01 12:08:57 -0500

    Con el Else, si no se cumple

--- Test.py
+++ Test.py
@@ -16,7 +16,7 @@ print("Hello World")
 mensaje = "El valor de la variable es"
 #IMPLEMENTAR LA FUNCION INPUT, Y EXPLICAR CADA PARAMETRO EFECTUADO
 #A = input("Escriba un numero para la variable A ")
-A = int(10)
+A = int(4)
 
 print(type(A), A)
 
@@ -60,7 +60,8 @@ print()
 if(A>5):
     print("Este es el resultado del if verdadero")
     print(f"La variable A es igual a: {A}")
-
+else:
+    print("La condicion no se cumplio")
 
 print()
 
@@ -68,7 +69,8 @@ print()
 if(B<11):
     print("Este es el resultado del if verdadero")
     print(f"La variable B es igual a: {B}")
-
+else:
+    print("La condicion no se cumplio")
 
 print()
 
@@ -76,7 +78,8 @@ print()
 if(C<=4):
     print("Este es el resultado del if verdadero")
     print(f"La variable C es igual a: {C}")
-    
+else:
+    print("La condicion no se cumplio")    
 
 print()
 
@@ -84,7 +87,7 @@ print()
 if(D>=55):
     print("Este es el resultado del if verdadero")
     print(f"La variable D es igual a: {D}")
-
+else:
+    print("La condicion no se cumplio")
 
 print()
-git config --global user.email "abetancourtt@unal.edu.co"
