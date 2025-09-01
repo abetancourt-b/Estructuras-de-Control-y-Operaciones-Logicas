c6a3f2df7724e49fa707d9e094d13c50f14e85d1
Antonio Betancourt <abetancourtt@unal.edu.co>
2025-09-01 12:20:58 -0500

    Con el condicional del If

---
+++ Estructuras de Control/Estructuras de Control.py
@@ -0,0 +1,50 @@
+#Autor: Antonio Betancourt
+
+A = 4
+
+B = 5
+
+C = 3
+
+D = 55
+
+
+print()
+
+# Estructuras de control, operaciones logicas
+#Booleano, 1,0,
+
+if(A>5):
+    print("Este es el resultado del if verdadero")
+    print(f"La variable A es igual a: {A}")
+else:
+    print("La condicion no se cumplio")
+
+print()
+
+
+if(B<11):
+    print("Este es el resultado del if verdadero")
+    print(f"La variable B es igual a: {B}")
+else:
+    print("La condicion no se cumplio")
+
+print()
+
+
+if(C<=4):
+    print("Este es el resultado del if verdadero")
+    print(f"La variable C es igual a: {C}")
+else:
+    print("La condicion no se cumplio")    
+
+print()
+
+
+if(D>=55):
+    print("Este es el resultado del if verdadero")
+    print(f"La variable D es igual a: {D}")
+else:
+    print("La condicion no se cumplio")
+
+print()
