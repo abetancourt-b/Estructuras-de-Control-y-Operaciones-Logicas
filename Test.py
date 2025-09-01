e98b23a23c90a440615b326e6e12b4e7d34234fa
Antonio Betancourt <abetancourtt@unal.edu.co>
2025-09-01 12:04:20 -0500

    Explicacion del If

---
+++ Test.py
@@ -0,0 +1,90 @@
+#Autor: Antonio Betancourt
+
+# 
+print("Hello World")
+
+# definicion a problemas
+
+# solucion a problemas
+
+#Algoritmos
+
+# encuentre el valor de x en la siguiente ecuacion
+
+# AX = B - Z
+
+mensaje = "El valor de la variable es"
+#IMPLEMENTAR LA FUNCION INPUT, Y EXPLICAR CADA PARAMETRO EFECTUADO
+#A = input("Escriba un numero para la variable A ")
+A = int(10)
+
+print(type(A), A)
+
+result = A + A 
+
+print(type(result), result)
+
+
+
+#1 Str a Int
+
+B = str(5)
+
+print("B inicial:", B, type(B))
+
+B = int(B)
+
+print("B convertido:", B, type(B))
+
+
+
+#2 Float a Int
+
+C = 3.1
+
+print("C inicial:", B, type(B))
+
+C = int(C)
+
+print("C convertido:", B, type(B))
+
+
+D = 55
+
+
+print()
+
+# Estructuras de control, operaciones logicas
+#Booleano, 1,0,
+
+if(A>5):
+    print("Este es el resultado del if verdadero")
+    print(f"La variable A es igual a: {A}")
+
+
+print()
+
+
+if(B<11):
+    print("Este es el resultado del if verdadero")
+    print(f"La variable B es igual a: {B}")
+
+
+print()
+
+
+if(C<=4):
+    print("Este es el resultado del if verdadero")
+    print(f"La variable C es igual a: {C}")
+    
+
+print()
+
+
+if(D>=55):
+    print("Este es el resultado del if verdadero")
+    print(f"La variable D es igual a: {D}")
+
+
+print()
+git config --global user.email "abetancourtt@unal.edu.co"
